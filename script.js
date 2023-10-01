d77c99acd7bc7a935f80d9a13a47b5cd8f43332d
dove <codegeekdove@gmail.com>
2023-10-01 21:02:44 +0800

    added dynamic shop page products

--- script.js
+++ script.js
@@ -28,11 +28,41 @@ const renderHomeProducts = () => {
   homeProductsEl.innerHTML = prodEight
 }
 
+// Render all items in Shop Page
+const renderShopPageItems = () => {
+  const shopPageProductsEl = document.getElementById('product-items')
+  const product = productsData
+    .map((product) => {
+      return `<div class="col-md-4 col-sm-6 mb-3">
+      <div class="card item">
+        <a href="item-details.html?id=${product.id}">
+          <div class="product-item">
+            <img src="${product.featureImg}" class="card-img-top radius-padding" alt="Cell Phone" />
+            <a role="button" class="btn btn-purple btn-block add-to-cart">ADD TO CART</a>
+          </div>
+        </a>
+        <a href="item-details.html?id=3">
+          <div class="card-body text-center">
+            <h5 class="card-title"><b>${product.title}</b></h5>
+            <p class="card-text price-color">₱ ${product.price}</p>
+          </div>
+        </a>
+      </div>
+    </div>`
+    })
+    .join('')
+
+  shopPageProductsEl.innerHTML = product
+}
+
 const renderWhichPage = () => {
   switch (currentPage) {
     case '/':
     case '/index.html':
       renderHomeProducts()
+      break
+    case '/products.html':
+      renderShopPageItems()
       break
   }
 }
